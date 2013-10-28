0b849bbd1767d7610865b7ed16b00675a644380d
Adrian Wolny <wolny101@gmail.com>
2013-10-28 23:13:46 +0100

    readme file + gradlew script for windows

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-mongodb-aggregation
+Sample web application demonstrating how to efficiently aggregate numerical data from multiple sources
+using MongoDB in order to perform real-time statistical analytics.
+
+Application can be used as a part of the monitoring subsystem in which
+we can specify some numerical resources to be monitored (e.g user session time, stock prices, etc.),
+and for these resources we want to compute statistics like: average and standard deviation.
+We also want the statistics to be available for different time range (minute, hour, day, etc.)
+and different groups (e.g users from different countries, stocks from different companies, etc.).
+These statistics must be computed almost instantly (from milliseconds up to a second) for a given time range.
+
+Application is based on Spring MVC deployed to Jetty web server.
+Two JSON REST services are exposed to the client:
+* to retrieve real-time statistics aggregated by hour for a given time range (URI template: /rest/{resource}/{start}/{end})
+* to collect numerical time series for a given resource (URI template: /rest/collector)
+
+To run the application type: gradlew jettyRunWar
+Application home page can be seen at: http://localhost:8080/kmug
+Date format to be used to specify time range in the main form: 'yyyy-MM-dd_HH:mm:ss'
+
+In order to generate normally distributed sample data for a given time range use SamplesGenerator class, for which you can specify:
+* the name of the resource to be analyzed
+* start date of the time range
+* end date of the time range
+* mean value of Gaussian distribution
+* standard deviation of Gaussian distribution
+
+then just run the application and run SamplesGenerator using your favourite IDE.
+
--- src/main/java/com/kmug/client/SamplesGenerator.java
+++ src/main/java/com/kmug/client/SamplesGenerator.java
@@ -18,19 +18,30 @@ import java.util.concurrent.TimeUnit;
  * @author Adrian Wolny
  */
 public class SamplesGenerator {
-
-
     public static void main(String[] args) throws IOException, ParseException {
+        // resource to be monitored
+        String resource = "xxx666";
+        // number of samples to be send to collector
+        int numberOfSamples = 500000;
+        // start date
+        String start = "2013-10-20";
+        // end date
+        String end = "2013-10-29";
+        // mean value of the Gaussian distribution
+        double mean = 100.0;
+        // standard deviation of the Gaussian distribution
+        double stdDev = 50.0;
+
+
         DefaultHttpClient httpClient = new DefaultHttpClient();
         ResponseHandler<Void> handler = new StringResponseHandler();
-
-        SamplesGenerator generator = new SamplesGenerator(100.0, 50.0, "2013-10-20", "2013-10-29");
+        SamplesGenerator generator = new SamplesGenerator(resource, start, end, mean, stdDev);
         double sum = 0.0;
         long count = 0;
-        for (int i = 0; i < COUNT; i++) {
-            long start = System.nanoTime();
+        for (int i = 0; i < numberOfSamples; i++) {
+            long startTime = System.nanoTime();
             httpClient.execute(generator.genPostRequest(), handler);
-            long dur = System.nanoTime() - start;
+            long dur = System.nanoTime() - startTime;
             sum += TimeUnit.MILLISECONDS.convert(dur, TimeUnit.NANOSECONDS);
             count++;
         }
@@ -38,8 +49,8 @@ public class SamplesGenerator {
         System.out.println("Avg. request processing time: " + (sum / count) + " ms");
     }
 
-    private static final int COUNT = 5000;
     public static final String CONTENT_TYPE = "application/json";
+    public static final String DATE_FORMAT = "yyyy-MM-dd";
 
     private static class StringResponseHandler implements ResponseHandler<Void> {
         @Override
@@ -49,10 +60,9 @@ public class SamplesGenerator {
         }
     }
 
-    private static final String DATE_FORMAT = "yyyy-MM-dd";
-    private static final String SAMPLE_RESOURCE = "xxx666";
 
     private final URI uri;
+    private final String resource;
     private final double mean;
     private final double stdDev;
     private final long start;
@@ -61,6 +71,10 @@ public class SamplesGenerator {
 
     public URI getUri() {
         return uri;
+    }
+
+    public String getResource() {
+        return resource;
     }
 
     public double getMean() {
@@ -83,8 +97,33 @@ public class SamplesGenerator {
         return mapper;
     }
 
-    public SamplesGenerator(double mean, double stdDev, String start, String end) throws ParseException {
+    /**
+     * Generates samples with random date within time range specified by <tt>start</tt> and <tt>end</tt>.
+     * Samples are generated with Gaussian distribution with mean 0.0 and standard deviation 1.0.
+     *
+     * @param resource name of the resource to be analyzed
+     * @param start    lower limit of the time range
+     * @param end      upper limit of the time range
+     * @throws ParseException if <tt>start</tt> or <tt>end</tt> does not comply with <em>yyyy-MM-dd</em> date format
+     */
+    public SamplesGenerator(String resource, String start, String end) throws ParseException {
+        this(resource, start, end, 0.0, 1.0);
+    }
+
+    /**
+     * Generates samples with random date within time range specified by <tt>start</tt> and <tt>end</tt>.
+     * Samples are generated with Gaussian distribution with mean <tt>mean</tt> and standard deviation <tt>stdDev</tt>.
+     *
+     * @param resource name of the resource to be analyzed
+     * @param start    lower limit of the time range
+     * @param end      upper limit of the time range
+     * @param mean     mean value of Gaussian function
+     * @param stdDev   standard deviation of Gaussian function
+     * @throws ParseException if <tt>start</tt> or <tt>end</tt> does not comply with <em>yyyy-MM-dd</em> date format
+     */
+    public SamplesGenerator(String resource, String start, String end, double mean, double stdDev) throws ParseException {
         uri = URI.create("http://localhost:8080/rest/collector");
+        this.resource = resource;
         this.mean = mean;
         this.stdDev = stdDev;
         this.start = new SimpleDateFormat(DATE_FORMAT).parse(start).getTime();
@@ -95,7 +134,7 @@ public class SamplesGenerator {
     private HttpPost genPostRequest() throws IOException {
         HttpPost result = new HttpPost(getUri());
         String json = getMapper().writeValueAsString(
-                new Sample(SAMPLE_RESOURCE,
+                new Sample(getResource(),
                         getMean() + getStdDev() * ThreadLocalRandom.current().nextGaussian(),
                         genRandomDate()));
         StringEntity input = new StringEntity(json);
